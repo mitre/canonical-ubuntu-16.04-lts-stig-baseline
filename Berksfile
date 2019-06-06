b6045951e667e3c48f504b07a6b53e1a2cacc6af
Aaron Lippold <lippold@gmail.com>
2019-06-06 18:26:10 -0400

    started work on kitchen and travis support
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

---
+++ Berksfile
@@ -0,0 +1,4 @@
+source 'https://supermarket.getchef.com'
+
+metadata
+cookbook 'os-hardening', github: 'dev-sec/chef-os-hardening'
---
+++ metadata.rb
@@ -0,0 +1,18 @@
+name             'stig' # ~FC121
+maintainer       'USGS WSI'
+maintainer_email 'isuftin@usgs.gov'
+license          'CPL-1.0'
+description      'Installs/Configures CIS STIG benchmarks'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.6.13'
+source_url       'https://github.com/USGS-CIDA/stig'
+issues_url       'https://github.com/USGS-CIDA/stig/issues'
+
+supports         'centos', '>= 6.6'
+supports         'centos', '>= 7.1'
+supports         'ubuntu'
+
+chef_version     '>= 12.0.0'
+
+depends       'logrotate', '~> 2.2.0'
+depends       'sysctl', '~> 0.10.2'
