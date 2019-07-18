72802b26212a518a2cf2ad479593672330f0240c
Krishna Kola <kkola@digitalinfuzion.com>
2019-07-18 11:39:16 -0400

    minor fixes

--- controls/V-75617.rb
+++ controls/V-75617.rb
@@ -86,13 +86,13 @@ Restart the audit service with the following command:
 
 # sudo systemctl restart auditd.service"
 
-    describe package('auditd') do
-      it { should be_installed }
-    end
-    describe service('auditd') do
-      it { should be_installed }
-      it { should be_enabled }
-      it { should be_running }
-    end
+  describe package('auditd') do
+    it { should be_installed }
+  end
+  describe service('auditd') do
+    it { should be_installed }
+    it { should be_enabled }
+    it { should be_running }
+  end
 end
 
